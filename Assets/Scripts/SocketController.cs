cb35abb29dbb244e8e266e5e7a1a5bc6538e5a7d
Imants Zarembo <imants.zarembo@gmail.com>
2022-06-12 01:47:25 +0300

    Update SocketController.cs

--- Assets/Scripts/SocketController.cs
+++ Assets/Scripts/SocketController.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class SocketController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform objectInitialParent = default;
+    private Vector3 objectInitialScale = default;
+    private XRSocketInteractor _socketI;
+
+    void Awake()
     {
-        
+        _socketI = gameObject.GetComponent<XRSocketInteractor>();
+        _socketI.selectEntered.AddListener(SelectEntered);
+        _socketI.selectExited.AddListener(SelectExited);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SelectEntered(SelectEnterEventArgs args)
     {
-        
+        IXRInteractable interactable = args.interactableObject;
+        objectInitialScale = interactable.transform.localScale;
+
+        objectInitialParent = interactable.transform.parent;
+        interactable.transform.parent = transform;
+        Debug.Log("SelectEntered");
+    }
+
+    private void SelectExited(SelectExitEventArgs args)
+    {
+        IXRInteractable interactable = args.interactableObject;
+        interactable.transform.localScale = objectInitialScale;
+
+        interactable.transform.parent = objectInitialParent;
+        Debug.Log("SelectExited");
     }
 }
