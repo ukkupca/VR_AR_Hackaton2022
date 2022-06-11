d012b543ef3c409784fa662107cb9e9306447325
Imants Zarembo <imants.zarembo@gmail.com>
2022-06-11 11:12:57 +0300

    Added zone barrier functionality

--- Assets/Scripts/ZoneController.cs
+++ Assets/Scripts/ZoneController.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ZoneController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private readonly int numberOfZones = 5;
+    private int[] zoneRadiuses = new int[6];
+    private bool[] zoneUnlocked = new bool[6];
+    public Tomogochi tomogochi = default;
+    public GameObject barrier = default;
+
     void Start()
     {
-        
+        zoneRadiuses[0] = 100;
+        zoneRadiuses[1] = 200;
+        zoneRadiuses[2] = 300;
+        zoneRadiuses[3] = 400;
+        zoneRadiuses[4] = 500;
+        zoneRadiuses[5] = 600;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (tomogochi != null)
+        {
+            for (int i = 0; i < numberOfZones; i++)
+            {
+                if (tomogochi.LEVEL - 1 >= i)
+                {
+                    zoneUnlocked[i] = true;
+                    barrier.transform.position = new Vector3(barrier.transform.position.x, zoneRadiuses[i], barrier.transform.position.z);
+                    barrier.transform.localScale = new Vector3(zoneRadiuses[i], zoneRadiuses[i], zoneRadiuses[i]);
+                }
+            }
+        }
     }
 }
